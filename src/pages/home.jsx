ae3d4f55897dd0eacb420e01a5e70cc326487dc0
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-17 17:53:51 -0300

    "fea: Home page created and method total "

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,42 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { Context } from '../context/context'
 import Header from '../componets/Header'
+import { Balance, CreateOperation } from '../styles/styleHome'
+import FormModal from '../componets/FormModal'
 const home = () => {
-	const { user,token, getOperationsAndUser } = useContext(Context)
+	const { user, token, getOperationsAndUser, operations } =
+		useContext(Context)
+	const [modal, setModal] = useState(false)
+	const total = (operations) =>
+		operations.reduce((acc, operation) => {
+			if (operation.incomOrExit == 'egreso')
+				return acc - +operation.amount
+			return acc + +operation.amount
+		}, 0)
+
 	useEffect(() => {
 		token && getOperationsAndUser()
 	}, [token])
 	return (
 		<>
 			<Header token={token} user={user ? user.email : ''} />
-      <div>
-        <div>
-          saldo : 0$
-        </div>
-      </div>
+			{token && (
+				<>
+					<div className="flex flex-column align-center">
+						<Balance>
+							<p>MI SALDO</p>
+							<span>$ {total(operations)}</span>
+						</Balance>
+						<CreateOperation onClick={() => setModal(true)}>
+							Create Operation
+						</CreateOperation>
+					</div>
+					{modal && <FormModal setModal={setModal} />}
+					{operations.map((e) => (
+						<div>{e.concept}</div>
+					))}
+				</>
+			)}
 		</>
 	)
 }
