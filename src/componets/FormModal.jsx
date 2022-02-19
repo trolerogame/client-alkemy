1b73039549aed7eb8aa5ea8dbe3034318aa36d0a
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 05:40:51 -0300

    "fix: change input to component InputModal"

--- src/componets/FormModal.jsx
+++ src/componets/FormModal.jsx
@@ -8,16 +8,22 @@ import {
 	ImgTime
 } from '../styles/styleHome'
 import { Context } from '../context/context'
+import InputModal from './InputModal'
+
 const FormModal = ({ setModal }) => {
 	const { createOperation } = useContext(Context)
 	const submitCreateOperation = async (e) => {
 		e.preventDefault()
-		const { concept, amount, incom } = e.target
-		if (concept.value && amount.value && incom.value) {
+		const { concept, amount, incom,date } = e.target
+		const data = [
+			concept.value , amount.value , incom.value , date.value
+		]
+		if (data.every(e => e)) {
 			await createOperation({
-				concept: concept.value,
-				amount: amount.value,
-				type: incom.value,
+				concept: data[0],
+				amount: data[1],
+				type:  data[2],
+			 	date: data[3]
 			})
 			setModal(false)
 		}
@@ -30,23 +36,18 @@ const FormModal = ({ setModal }) => {
 				className="flex flex-column align-center"
 			>
 				<div className="concept-amount flex flex-column align-center">
-					<div className='my-6'>
-						<label htmlFor="">Concept</label>
-						<input type="text" name="concept" />
-					</div>
-					<div className='my-6'>
-						<label htmlFor="">Amount</label>
-						<input type="number" name="amount" />
-					</div>
+					<InputModal type="text" name='concept' id='Concept' />
+					<InputModal type="number" name='amount' id='Amount' />
+					<InputModal type="datetime-local" name='date' id='Date' />
 				</div>
 				<div className="flex">
 					<ContainRatio className="flex flex-column align-center">
 						<label htmlFor="ingreso">Income</label>
-						<input type="radio" name="incom" value="ingreso" />
+						<input type="radio" name="incom" id='ingreso' value="ingreso" />
 					</ContainRatio>
 					<ContainRatio className="flex flex-column align-center">
 						<label htmlFor="egreso">Exit</label>
-						<input type="radio" name="incom" value="egreso" />
+						<input type="radio" name="incom" id='egreso' value="egreso" />
 					</ContainRatio>
 				</div>
 				<CreateOperationSubmit>CREATE OPERATION</CreateOperationSubmit>
