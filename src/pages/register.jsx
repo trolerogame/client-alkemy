a781b43284d3697824b2c79bc2a1b3a8cb9bf7c3
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 05:35:39 -0300

    "fix: included input username"

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useEffect } from 'react'
 import { Context } from '../context/context'
 import { navigate } from '@reach/router'
 import FormLoginRegister from '../componets/FormLoginRegister'
+import { ContainLabel } from '../styles/styleLoginRegister'
 const register = () => {
 	const { createUser, token } = useContext(Context)
 
 	const submitRegister = async (e) => {
 		e.preventDefault()
-		const { email, password } = e.target
-		const error = await createUser(email.value, password.value)
+		const { email, password,username } = e.target
+		const error = await createUser(email.value, password.value,username.value)
 		if (error) return
 		navigate('/login')
 	}
@@ -22,7 +23,17 @@ const register = () => {
 			button="SIGN UP"
 			redirect="login"
 			text="you adready register?"
-		/>
+		>
+			<ContainLabel className="flex flex-column">
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					name="username"
+					id="username"
+					placeholder="Username"
+				/>
+			</ContainLabel>
+		</FormLoginRegister>
 	)
 }
 
