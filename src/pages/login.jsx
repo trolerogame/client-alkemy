b4ad5f166dc197da5772d65fa28a4dda5d06a8dd
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 20:13:07 -0300

    "fea: error includes form"

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,27 @@
 import { navigate } from '@reach/router'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import { Context } from '../context/context'
 import { setTokenCookie } from '../utils/setTokenCookie'
 import FormLoginRegister from '../componets/FormLoginRegister'
 const login = () => {
+	
 	const { loginUser, token } = useContext(Context)
+	const [validate, setValidate] = useState({
+		Email: null,
+		Password: null,
+	})
+	const [error,setError] = useState('')
 	const submitLogin = async (e) => {
 		e.preventDefault()
 		const { email, password } = e.target
-		if (!email.value && !password.value) return
+		if (!validate.Email && !validate.Password) return
 		const token = await loginUser(email.value, password.value)
-		if (token.error) return
+		if (token.error) return setError(token.error)
+		setError('')
 		setTokenCookie(token.token)
 		document.location.href = '/'
 	}
+
 	useEffect(() => {
 		if (token) navigate('/')
 	}, [])
@@ -23,7 +31,10 @@ const login = () => {
 			submit={submitLogin}
 			button="SIGN IN"
 			redirect="register"
+			error={error}
 			text="Have no account?"
+			setValidate={setValidate}
+			validate={validate}
 		/>
 	)
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,16 +1,29 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import { Context } from '../context/context'
 import { navigate } from '@reach/router'
 import FormLoginRegister from '../componets/FormLoginRegister'
 import { ContainLabel } from '../styles/styleLoginRegister'
+import Input from '../componets/Input'
 const register = () => {
 	const { createUser, token } = useContext(Context)
-
+	const [validate, setValidate] = useState({
+		Email: null,
+		Password: null,
+		Username: null,
+	})
+	const [error,setError] = useState('')
 	const submitRegister = async (e) => {
 		e.preventDefault()
-		const { email, password,username } = e.target
-		const error = await createUser(email.value, password.value,username.value)
-		if (error) return
+		const { email, password, text } = e.target
+		if (!validate.Email && !validate.Password && !validate.Username) return
+		
+		const error = await createUser(
+			email.value,
+			password.value,
+			text.value
+		)
+		if (error) return setError(error)
+		setError('')
 		navigate('/login')
 	}
 	useEffect(() => {
@@ -23,15 +36,12 @@ const register = () => {
 			button="SIGN UP"
 			redirect="login"
 			text="you adready register?"
+			error={error}
+			validate={validate}
+			setValidate={setValidate}
 		>
-			<ContainLabel className="flex flex-column">
-				<label htmlFor="username">Username</label>
-				<input
-					type="text"
-					name="username"
-					id="username"
-					placeholder="Username"
-				/>
+			<ContainLabel validate={validate.Username} className="flex flex-column">
+				<Input placeholder="Username" type="text" setValidate={setValidate} validate={validate} />
 			</ContainLabel>
 		</FormLoginRegister>
 	)
