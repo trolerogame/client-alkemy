6d7ac2f972bb6600c78cd5897f95ebe99e7710ed
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 05:50:51 -0300

    "fea: include button user and logout"

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,15 +1,31 @@
-import { Link } from '@reach/router'
-import React from 'react'
-import { Header,LinkStyle } from '../styles/styleGlobal'
-const header = ({ token, user }) => {
+import React, { useState } from 'react'
+import { Header, LinkStyle, ButtonDropdown,HeaderInfoUser } from '../styles/styleGlobal'
+const header = ({ token, user, logout }) => {
+	const [rotate, setRotate] = useState(false)
 	return (
-		<Header className='flex align-center justify-between'>
+		<Header className="flex align-center justify-between">
 			<img src="/alkemy-logo.svg" alt="" />
 			{token ? (
-				<>{user}</>
+				<>
+					<HeaderInfoUser
+
+						className="flex align-center"
+						onClick={() => setRotate(!rotate)}
+						rotate={rotate ? rotate : undefined}
+					>
+						<p>
+							{user && user.length > 10
+								? user.split('').slice(0, 10).join('')
+								: user}
+						</p>
+						<ButtonDropdown onClick={() => logout()}>
+							Log Out
+						</ButtonDropdown>
+					</HeaderInfoUser>
+				</>
 			) : (
 				<>
-					<div className='flex'>
+					<div className="flex">
 						<LinkStyle to="/register">Sign Up</LinkStyle>
 						<LinkStyle to="/login">Sign In</LinkStyle>
 					</div>
