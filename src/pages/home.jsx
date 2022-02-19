b7417f2cb81250efbc84ebdbe9ab7143dab4a07e
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 20:12:38 -0300

    "fea:created button incom or exit operations"

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useContext, useState } from 'react'
 import { Context } from '../context/context'
 import Header from '../componets/Header'
-import { Balance, CreateOperation,ContainHome } from '../styles/styleHome'
+import {
+	Balance,
+	CreateOperation,
+	ContainHome,
+	IncomOrExit,
+} from '../styles/styleHome'
 import FormModal from '../componets/FormModal'
 import OperationItem from '../componets/OperationItem'
 import { balanceAmount } from '../utils/balanceAmount'
@@ -9,6 +14,7 @@ const home = () => {
 	const { user, token, getOperationsAndUser, operations, logout } =
 		useContext(Context)
 	const [modal, setModal] = useState(false)
+	const [incom, setIncom] = useState(false)
 	useEffect(() => {
 		token && getOperationsAndUser()
 	}, [token])
@@ -20,7 +26,7 @@ const home = () => {
 				user={user ? user.username : ''}
 			/>
 			{token && (
-				<ContainHome className='flex'>
+				<ContainHome className="flex">
 					<div className="flex flex-column align-center w-50">
 						<Balance>
 							<p>MI SALDO</p>
@@ -31,8 +37,24 @@ const home = () => {
 						</CreateOperation>
 						{modal && <FormModal setModal={setModal} />}
 					</div>
-					<div className='w-50'>
-						{operations.map((e) => (
+					<div className="w-50">
+						<div className="flex justify-center">
+							<IncomOrExit
+								incom={incom}
+								onClick={() => setIncom(false)}
+							>
+								INCOM
+							</IncomOrExit>
+							<IncomOrExit
+								incom={!incom}
+								onClick={() => setIncom(true)}
+							>
+								EXIT
+							</IncomOrExit>
+						</div>
+						{operations.filter(e =>  { 
+							return !incom ? e.incomOrExit == 'ingreso' : e.incomOrExit == 'egreso'
+						}).map((e) => (
 							<OperationItem key={e.id} {...e} />
 						))}
 					</div>
