1ddd547996455a7efc2a709dde3ae9d5f5480cc8
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 20:11:22 -0300

    "fea: create reg expression and error message for form"

---
+++ src/utils/regForm.js
@@ -0,0 +1,11 @@
+export const reg = {
+    text:/^([A-Za-z0-9])\w+/g,
+    email:/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g,
+    password:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/g,
+}
+
+export const error = {
+    text:'The user must be more than two characters long',
+    email:'The email is incorrect',
+    password:'the password must contain letters, a capital letter, a special character and numbers',
+}
