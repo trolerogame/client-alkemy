1c5b4b70b4c2fcc0381bde2fd5f2a87d3517136d
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 20:13:59 -0300

    "fix: change input to component input and add error message"

--- src/componets/FormLoginRegister.jsx
+++ src/componets/FormLoginRegister.jsx
@@ -1,34 +1,59 @@
 import React from 'react'
 import { Link } from '@reach/router'
-import { ContainCard, Card,ContainInputs, ContainLabel,Button } from '../styles/styleLoginRegister'
-const FormLoginRegister = ({ submit, button, redirect,text, children }) => {
+import {
+	ContainCard,
+	Card,
+	ContainInputs,
+	ContainLabel,
+	Button,
+	ErrorMessage
+} from '../styles/styleLoginRegister'
+import Input from './Input'
+const FormLoginRegister = ({
+	submit,
+	button,
+	redirect,
+	text,
+	children,
+	validate,
+	setValidate,
+	error
+}) => {
 	return (
 		<ContainCard>
 			<Card onSubmit={submit} className="flex flex-column justify-center">
 				<h2>{button}</h2>
+				{error && <ErrorMessage center>{error}</ErrorMessage>}
 				<ContainInputs>
 					{children}
-					<ContainLabel className="flex flex-column">
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							name="email"
-							id="email"
+					<ContainLabel
+						className="flex flex-column"
+						validate={validate.Email}
+					>
+						<Input
 							placeholder="Email"
+							type="email"
+							setValidate={setValidate}
+							validate={validate}
 						/>
 					</ContainLabel>
-					<ContainLabel className="flex flex-column">
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							name="password"
-							id="password"
+					<ContainLabel
+						className="flex flex-column"
+						validate={validate.Password}
+						setValidate={setValidate}
+					>
+						<Input
 							placeholder="Password"
+							type="password"
+							setValidate={setValidate}
+							validate={validate}
 						/>
 					</ContainLabel>
 				</ContainInputs>
 				<Button>{button}</Button>
-				<p>{text} <Link to={'/' + redirect}>{redirect}</Link></p>
+				<p>
+					{text} <Link to={'/' + redirect}>{redirect}</Link>
+				</p>
 			</Card>
 		</ContainCard>
 	)
