020f5733c3b56bcb76041161129aa9e4965d4b7c
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-21 05:41:05 -0300

    fix: get token name

--- src/utils/getTokenCookie.js
+++ src/utils/getTokenCookie.js
@@ -2,6 +2,6 @@
 export const getTokenCookie = () => {
 	for (let cookie of document.cookie.split('; ')) {
         cookie = cookie.split('=')
-		if (cookie[0] == 'token') return cookie[1]
+		if (cookie[0] == 'token_operations') return cookie[1]
 	}
 }
